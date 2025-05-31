f93565b1b7f44e0b6811ac9b1c6562a1e65848e6
Matin.Kh <149156580+MatinKhademian@users.noreply.github.com>
2025-05-31 09:51:07 +0330

    add many to many relationship

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-
-// use App\Http\Controllers\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
- Route::get('/', function () {
-     return view('welcome');
- });
-
-Route::group(['prefix' => '/panel', 'middleware' => ['auth:web', \App\Http\Middleware\LogUserActivity::class]], function () {
-    Route::view('/show', "panel.index")->name('panel1');
-    Route::get('/users/{user}/logs', [UserController::class, 'logs'])->name('logs');
-    Route::resource('users', UserController::class)->only(['index', 'store', 'edit', 'update', 'destroy', 'create']);
-    Route::resource('categories', CategoryController::class);
-    Route::resource('products', ProductController::class);
-    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-});
-
-
-Route::group(['middleware' => 'guest'], function () {
-Route::view('login', "panel.login")->name('login');
-Route::post('login', [AuthController::class,'doLogin'])->name('do.login');
-
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
